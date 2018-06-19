17ec86b0f17941e75b602f60fdffcc404a92b368
Chris Fritz <chrisvfritz@gmail.com>
2018-06-19 19:26:13 -0400

    initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-center text-blue">
+      {{ greeting }}! Let's prototype.
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~tailwindcss/dist/tailwind.css';
+</style>
